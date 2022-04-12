13e4cc5036d5270b5a7aef5f9dcd7e75e3266cb3
TemidoRocha <manueltemidorocha@gmail.com>
2022-04-12 09:44:00 +0100

    babel implementation

---
+++ DevEnvironment/WebPack/README.md
@@ -0,0 +1,8 @@
+# webpack.config.js
+
+This file will controle the ocnfiguration of the webpack.
+It allows to add modules like the babel module but it doesn't control totally that module.
+
+# .babelrc
+
+We need this file to tell babel how it shoudl work
--- DevEnvironment/WebPack/webpack.config.js
+++ DevEnvironment/WebPack/webpack.config.js
@@ -1,7 +1,23 @@
+// https://webpack.js.org/configuration/
+
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js%/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
