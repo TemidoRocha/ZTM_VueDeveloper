07b1494a0f4bc89f52756f47b9e9075edc49da4e
TemidoRocha <manueltemidorocha@gmail.com>
2022-04-13 12:00:05 +0100

    Quiz
    Structure and animations

---
+++ quiz/src/components/QuizResult.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="result">
+    <div class="title">
+      You got sample result {{ results[resultIndex].title }}!
+    </div>
+    <div class="desc">{{ results[resultIndex].desc }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuizResult",
+  props: ["results", "totalCorrect"],
+  computed: {
+    resultIndex() {
+      let index = 0;
+
+      this.results.forEach((item, i) => {
+        if (item.min <= this.totalCorrect && item.max >= this.totalCorrect) {
+          index = i;
+        }
+      });
+      return index;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
