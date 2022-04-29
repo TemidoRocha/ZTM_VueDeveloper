049db4ab25228b21e4fd567214769b892912839c
TemidoRocha <manueltemidorocha@gmail.com>
2022-04-29 17:52:05 +0100

    UPDATING FIREBASE

---
+++ music/src/components/CompositionItem.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="border border-gray-200 p-3 mb-4 rounded">
+    <div v-show="!showForm">
+      <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
+      <button
+        class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
+      >
+        <i class="fa fa-times"></i>
+      </button>
+      <button
+        class="
+          ml-1
+          py-1
+          px-2
+          text-sm
+          rounded
+          text-white
+          bg-blue-600
+          float-right
+        "
+        @click.prevent="showForm = !showForm"
+      >
+        <i class="fa fa-pencil-alt"></i>
+      </button>
+    </div>
+    <div v-show="showForm">
+      <div
+        class="text-white text-center font-bold p4 mb-4"
+        v-if="show_alert"
+        :class="alert_variant"
+      >
+        {{ alert_message }}
+      </div>
+      <vee-form
+        :validation-schema="schema"
+        :initial-values="song"
+        @submit="edit"
+      >
+        <div class="mb-3">
+          <label for="songTitle" class="inline-block mb-2"
+            >Song Title
+            <vee-field
+              id="songTitle"
+              name="modified_name"
+              type="text"
+              class="
+                block
+                w-full
+                py-1.5
+                px-3
+                text-gray-800
+                border border-gray-300
+                transition
+                duration-500
+                focus:outline-none focus:border-black
+                rounded
+              "
+              placeholder="Enter Song Title"
+            />
+            <ErrorMessage class="text-red-600" name="modified_name" />
+          </label>
+        </div>
+        <div class="mb-3">
+          <label for="genre" class="inline-block mb-2"
+            >Genre
+            <vee-field
+              id="genre"
+              name="genre"
+              type="text"
+              class="
+                block
+                w-full
+                py-1.5
+                px-3
+                text-gray-800
+                border border-gray-300
+                transition
+                duration-500
+                focus:outline-none focus:border-black
+                rounded
+              "
+              placeholder="Enter Genre"
+            />
+            <ErrorMessage class="text-red-600" name="genre" />
+          </label>
+        </div>
+        <button
+          type="submit"
+          class="py-1.5 px-3 rounded text-white bg-green-600"
+          :disabled="in_submission"
+        >
+          Submit
+        </button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+          @click.prevent="showForm = false"
+          :disabled="in_submission"
+        >
+          Go Back
+        </button>
+      </vee-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { songsCollection } from '@/includes/firebase';
+
+export default {
+  name: 'CompositionItem',
+  props: {
+    song: {
+      type: Object,
+      required: true,
+    },
+    updateSong: {
+      type: Function,
+      required: true
+    },
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      showForm: false,
+      schema: {
+        modified_name: 'required',
+        genre: 'alphaSpaces',
+      },
+      in_submission: false,
+      show_alert: false,
+      variant: 'bg-blue-500',
+      alert_message: 'Please wait! Updating song info.',
+    };
+  },
+  methods: {
+    async edit(values) {
+      this.in_submission = true;
+      this.show_alert = true;
+      this.alert_variant = 'bg-blue_500';
+      this.alert_message = 'Please wait! Updating song info.';
+
+      try {
+        await songsCollection.doc(this.song.docId).update(values);
+      } catch (error) {
+        this.in_submission = false;
+        this.alert_variant = 'bg-red';
+        this.alert_message = 'Something went wrong! Try again later';
+      }
+      this.updateSong(this.index, values);
+      this.in_submission = false;
+      this.alert_variant = 'bg-green';
+      this.alert_message = 'Success';
+    },
+  },
+};
+</script>
