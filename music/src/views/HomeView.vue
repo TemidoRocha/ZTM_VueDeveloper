b43c36697fb50019006334ef49b08fed29c95389
TemidoRocha <manueltemidorocha@gmail.com>
2022-04-30 22:19:22 +0100

    ROUTE TRANSITIONS

--- music/src/views/HomeView.vue
+++ music/src/views/HomeView.vue
@@ -1,44 +1,50 @@
 <template>
-  <!-- Introduction -->
-  <section class="mb-8 py-20 text-white text-center relative">
-    <div
-      class="absolute inset-0 w-full h-full bg-contain introduction-bg"
-      style="background-image: url(assets/img/header.png)"
-    ></div>
-    <div class="container mx-auto">
-      <div class="text-white main-header-content">
-        <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
-        <p class="w-full md:w-8/12 mx-auto">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et
-          dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
-          suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
-          pulvinar, fringilla lorem eget, ullamcorper urna.
-        </p>
+  <main>
+    <!-- Introduction -->
+    <section class="mb-8 py-20 text-white text-center relative">
+      <div
+        class="absolute inset-0 w-full h-full bg-contain introduction-bg"
+        style="background-image: url(assets/img/header.png)"
+      ></div>
+      <div class="container mx-auto">
+        <div class="text-white main-header-content">
+          <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
+          <p class="w-full md:w-8/12 mx-auto">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            et dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
+            suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
+            pulvinar, fringilla lorem eget, ullamcorper urna.
+          </p>
+        </div>
       </div>
-    </div>
 
-    <img
-      class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-      src="assets/img/introduction-music.png"
-      alt="music introduction"
-    />
-  </section>
+      <img
+        class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
+        src="assets/img/introduction-music.png"
+        alt="music introduction"
+      />
+    </section>
 
-  <!-- Main Content -->
-  <section class="container mx-auto">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <span class="card-title">Songs</span>
-        <!-- Icon -->
-        <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
+    <!-- Main Content -->
+    <section class="container mx-auto">
+      <div
+        class="bg-white rounded border border-gray-200 relative flex flex-col"
+      >
+        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+          <span class="card-title">Songs</span>
+          <!-- Icon -->
+          <i
+            class="fa fa-headphones-alt float-right text-green-400 text-xl"
+          ></i>
+        </div>
+        <!-- Playlist -->
+        <ol id="playlist">
+          <song-item v-for="song in songs" :key="song.docId" :song="song" />
+        </ol>
+        <!-- .. end Playlist -->
       </div>
-      <!-- Playlist -->
-      <ol id="playlist">
-        <song-item v-for="song in songs" :key="song.docId" :song="song" />
-      </ol>
-      <!-- .. end Playlist -->
-    </div>
-  </section>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -66,10 +72,19 @@ export default {
          * in order to implement the infinite scrolling we need to sort the doc and then
          * we are able to use the startAfter fn
          */
-        const lastDoc = await songsCollection.doc(this.songs[this.songs.length - 1].docId).get();
-        snapshot = await songsCollection.orderBy('modified_name').startAfter(lastDoc).limit(this.maxPerPage).get();
+        const lastDoc = await songsCollection
+          .doc(this.songs[this.songs.length - 1].docId)
+          .get();
+        snapshot = await songsCollection
+          .orderBy('modified_name')
+          .startAfter(lastDoc)
+          .limit(this.maxPerPage)
+          .get();
       } else {
-        snapshot = await songsCollection.orderBy('modified_name').limit(this.maxPerPage).get();
+        snapshot = await songsCollection
+          .orderBy('modified_name')
+          .limit(this.maxPerPage)
+          .get();
       }
       snapshot.forEach((document) => {
         const song = {
@@ -88,11 +103,14 @@ export default {
       if (bottomOfWindow) {
         this.getSongs();
       }
-    }
+    },
   },
   async created() {
     let numberOfSongs = this.songs.length;
-    while (document.documentElement.clientHeight === document.documentElement.scrollHeight) {
+    while (
+      document.documentElement.clientHeight
+      === document.documentElement.scrollHeight
+    ) {
       // eslint-disable-next-line no-await-in-loop
       await this.getSongs();
       if (numberOfSongs === this.songs.length) {
@@ -106,6 +124,6 @@ export default {
   beforeUnmount() {
     // removing the event listner is important to prevent memory leak
     window.removeEventListener('scroll', this.handleScroll);
-  }
+  },
 };
 </script>
