c2857fe935c55a489e9e78a6a262fe51120c612a
TemidoRocha <manueltemidorocha@gmail.com>
2022-05-01 12:49:49 +0100

    i18n PLUGIN INSTALATION
    structure change for scalability

--- music/src/main.js
+++ music/src/main.js
@@ -7,7 +7,7 @@ import { auth } from './includes/firebase';
 import Icon from './directives/icon';
 import './assets/tailwind.css';
 import './assets/main.css';
-import i18n from './i18n';
+import i18n from './includes/i18n';
 
 let app;
 
