d3233f89d97609a55c41f1efef2cee4eeece6db9
TemidoRocha <manueltemidorocha@gmail.com>
2022-05-03 11:02:23 +0100

    TAIWIND CONFIG
    warn - The `purge`/`content` options have changed in Tailwind CSS v3.0.
    warn - Update your configuration file to eliminate this warning.
    warn - https://tailwindcss.com/docs/upgrade-guide#configure-content-sources

--- music/tailwind.config.js
+++ music/tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: [
-      './public/**/*.html', './src/**/*.vue'
-    ]
-  },
+  content: [
+    './public/**/*.html', './src/**/*.vue'
+  ],
   theme: {
     extend: {}
   },
